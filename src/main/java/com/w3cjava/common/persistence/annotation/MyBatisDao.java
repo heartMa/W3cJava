9e12faea5df41ce3d5bbcdf3ead15c48d6197e72
891091835 <891091835@qq.com>
2018-04-21 15:41:38 +0800

    基础的SSM整合

---
+++ src/main/java/com/w3cjava/common/persistence/annotation/MyBatisDao.java
@@ -0,0 +1,34 @@
+package com.w3cjava.common.persistence.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @version v1.0.0
+ * @author heartma
+ * @date 2017年12月27日
+ * @File MyBatisDao.java
+ * @URL www.w3cjava.com
+ * @Desc 标识MyBatis的DAO,方便
+ *       {@link org.mybatis.spring.mapper.MapperScannerConfigurer}的扫描
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Component
+public @interface MyBatisDao {
+
+	/**
+	 * The value may indicate a suggestion for a logical component name, to be
+	 * turned into a Spring bean in case of an autodetected component.
+	 * 
+	 * @return the suggested component name, if any
+	 */
+	String value() default "";
+
+}
